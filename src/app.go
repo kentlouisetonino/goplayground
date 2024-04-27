763de3c5c678ac17c0821b6d1942747f92d5f4e8
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-04-27 23:20:23 +0800

    Initialize a server.

---
+++ src/app.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hello World!")
+	})
+
+	s := &http.Server{
+		Addr:           ":11000",
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatal(s.ListenAndServe())
+}
