9752a91228e7d49f4a68dbf336e475006e5af42f
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-04-30 18:33:40 +0800

    Useless channge.

--- src/routes/aws-route.go
+++ src/routes/aws-route.go
@@ -6,7 +6,7 @@ import (
 )
 
 func AWSRoute(w http.ResponseWriter, r *http.Request) {
-	// api/aws/s3/upload
+	fmt.Println(r.URL.Path)
 	if r.Method == http.MethodPost && r.URL.Path == "/s3/upload" {
 		fmt.Fprintf(w, "Hello Hell!")
 	}
