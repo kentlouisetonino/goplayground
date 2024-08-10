e9e0b59c55db5b2e40c2f142a83d3b4a1857b461
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-08-10 16:07:11 +0800

    Include a data response.

--- src/models/http-response.go
+++ src/models/http-response.go
@@ -1,6 +1,7 @@
 package models
 
 type HTTPResponse struct {
-	Message    string `json:"message"`
-	StatusCode int    `json:"status_code"`
+	Message    string       `json:"message"`
+	StatusCode int          `json:"status_code"`
+	Data       *interface{} `json:"data"`
 }
